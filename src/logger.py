d886fc1225b1faed87fef8eeda838cfba6a54470
Ayushman Kumar Pandey <kumarayushmanpandey075@gmail.com>
2025-07-14 09:43:24 +0530

    Initial commit

---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
+logging.basicConfig(
+    filename=log_path,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
